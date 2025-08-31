47af9894cb86a3eb8e16f94414c3c21681025b99
mahamat-ali <terapfils30@gmail.com>
2025-08-31 21:54:43 +0100

    Set up Shadcn and configure prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from 'react'
-import './index.css'
+import { useEffect, useState } from 'react';
+import './index.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return <p className='text-3xl'>{message}</p>
-
+   return (
+      <div className="p-8">
+         <p className="text-3xl">{message}</p>
+         <Button>Click me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
+   plugins: [react(), tailwindcss()],
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
       },
-    },
-  },
+   },
+   server: {
+      proxy: {
+         '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+            secure: false,
+         },
+      },
+   },
 });
